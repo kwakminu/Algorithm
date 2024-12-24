97ce9c0c3e9e42cd3edd0dd18d8de885f6c04598
KMW <129249419+kwakminu@users.noreply.github.com>
2024-12-24 12:59:52 +0900

    [Bronze V] Title: 구구단, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2739. 구구단/구구단.cc
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int a;
+
+    cin >> a;
+
+    for (int i = 1; i <= 9; i++){
+        cout << a << " * " << i << " = " << a * i << "\n";
+    }
+
+
+}
