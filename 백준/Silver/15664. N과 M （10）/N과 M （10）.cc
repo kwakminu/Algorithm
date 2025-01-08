f74ae5bf2959d6066c5a328b542957ed344f0e1b
KMW <129249419+kwakminu@users.noreply.github.com>
2025-01-08 22:19:10 +0900

    [Silver II] Title: N과 M (10), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15664. N과 M （10）/N과 M （10）.cc
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int n, m;
+int num[10]; // 입력한 수열
+int arr[10]; // 정리해논 수열
+bool isused[10]; // 입력한 수열 인덱스 사용했는지
+
+void func(int k){
+  if (k == m){
+    for (int i = 0; i < m; i ++){
+      cout << arr[i] << ' ';
+    }
+    cout << '\n';
+    return;
+  }
+  int tmp = 0; // 중복 순열인지 확인하기 위해 필요한 임시 변수
+  for (int i = 0; i < n; i++){
+    if (k != 0 && arr[k-1] > num[i]) continue;
+    if (!isused[i] && tmp != num[i]){
+      arr[k] = num[i];
+      isused[i] = true;
+      tmp = arr[k];
+      func(k + 1);
+      isused[i] = false;
+    }
+  }
+
+}
+
+
+int main(void){
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  cin >> n >> m;
+  for (int i = 0; i < n; i ++){
+    cin >> num[i];
+  }
+  sort(num,num + n);
+  func(0);
+}
