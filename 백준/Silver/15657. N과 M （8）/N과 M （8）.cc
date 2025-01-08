31f52244547ad146370b3bd7f10d58e4bbb4bd01
KMW <129249419+kwakminu@users.noreply.github.com>
2025-01-08 21:17:29 +0900

    [Silver III] Title: N과 M (8), Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15657. N과 M （8）/N과 M （8）.cc
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int n, m;
+int inp[10];
+int arr[10];
+void func(int k){
+  if (k == m){
+    for (int i = 0; i < m; i ++){
+      cout << arr[i] << ' ';
+    }
+    cout << '\n';
+    return;
+  }
+  for (int i = 0; i < n; i++){
+    if (k != 0 && arr[k-1] > inp[i]) continue;
+    arr[k] =  inp[i];
+    func(k + 1);
+  }
+
+}
+
+
+int main(void){
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  cin >> n >> m;
+  for (int i = 0; i < n; i ++){
+    cin >> inp[i];
+  }
+  sort(inp,inp + n);
+  func(0);
+}
