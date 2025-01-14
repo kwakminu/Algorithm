cb3a53e2209f31ffc8988957a36e8edac9f539c0
KMW <129249419+kwakminu@users.noreply.github.com>
2025-01-14 17:49:28 +0900

    [Silver III] Title: 먹을 것인가 먹힐 것인가, Time: 28 ms, Memory: 2440 KB -BaekjoonHub

---
+++ 백준/Silver/7795. 먹을 것인가 먹힐 것인가/먹을 것인가 먹힐 것인가.cc
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int main(void){
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  int t;
+  cin >> t;
+  while (t--){
+    int n, m;
+    cin >> n >> m;
+    vector<pair<int, int>> v(n+m);
+    for (int i = 0; i < n; i++){
+      int a;
+      cin >> a;
+      v[i] = {a, 0};
+    }
+    for (int i = n; i < n+m; i++){
+      int b;
+      cin >> b;
+      v[i] = {b,1};
+    }
+    sort(v.begin(), v.end());
+    int cnt = 0;
+    int ans = 0;
+    for (int i = 0; i < n+m; i ++){
+      if (v[i].second == 0) // 현재 보는 수가 A에 속한 수
+        ans += cnt;
+      else 
+        cnt++;
+    }
+    cout << ans << '\n';
+  }
+ 
+
+}
