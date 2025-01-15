af196931f7e17b5cef9488c8e11d91c4e6d76413
KMW <129249419+kwakminu@users.noreply.github.com>
2025-01-15 11:03:06 +0900

    [Silver III] Title: 구간 합 구하기 4, Time: 36 ms, Memory: 2800 KB -BaekjoonHub

---
+++ 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.cc
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int n, m;
+int d[100005];
+int a[100005];
+
+int main(void){
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+
+  cin >> n >> m;
+  for (int i = 1; i <= n; i++){
+    cin >> a[i];
+  }
+  d[1] = a[1];
+  for (int i = 2; i <= n; i++){
+    d[i] = d[i-1] + a[i];
+  }
+  while(m--){
+    int i, j;
+    cin >> i >> j;
+    cout << d[j] - d[i-1] << '\n';
+  }
+}
