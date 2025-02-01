08a888773de0c6210fcb5ee114542be3f194e358
KMW <129249419+kwakminu@users.noreply.github.com>
2025-02-01 19:36:59 +0900

    [Bronze V] Title: 개, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10172. 개/개.cc
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(void)
+{
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+
+  cout << "|\\_/|\n";
+  cout << "|q p|   /}\n";
+  cout << "( 0 )\"\"\"\\\n";
+  cout << "|\"^\"`    |\n";
+  cout << "||_/=\\\\__|";
+}
