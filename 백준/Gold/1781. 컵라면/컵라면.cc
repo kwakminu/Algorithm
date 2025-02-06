3514b947e213f01f16dc507d835fe130a9f36f8d
KMW <129249419+kwakminu@users.noreply.github.com>
2025-02-06 16:14:42 +0900

    [Gold II] Title: 컵라면, Time: 64 ms, Memory: 5252 KB -BaekjoonHub

---
+++ 백준/Gold/1781. 컵라면/컵라면.cc
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define X first
+#define Y second
+
+priority_queue<int, vector<int>, greater<int>> pq;
+pair<int, int> problem[200002];
+
+int main(void)
+{
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  int n;
+  cin >> n;
+  for (int i = 0; i < n; i ++){
+    int a, b;
+    cin >> a >> b;
+    problem[i] = {a, b};
+  }
+  sort(problem, problem + n);
+  
+  for (int i = 0; i < n; i++){
+    pq.push(problem[i].Y);
+    if (pq.size() > problem[i].X){
+      pq.pop();
+    }
+  }
+
+  int ans = 0;
+  while(!pq.empty()){
+    ans += pq.top();
+    pq.pop();
+  }
+  cout << ans;
+
+}
