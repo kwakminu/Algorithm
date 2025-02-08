e0a0e695cf4de4365bc28e835acb0258e42befd7
KMW <129249419+kwakminu@users.noreply.github.com>
2025-02-08 15:02:20 +0900

    [Bronze V] Title: 문자와 문자열, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/27866. 문자와 문자열/문자와 문자열.cc
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int main(void)
+{
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+
+  string s;
+  cin >> s;
+  int i;
+  cin >> i;
+  cout << s[i-1] << '\n';
+}
+ 
