a1e7bad7cc7552d510c27bc113d7d438d0748eb5
KMW <129249419+kwakminu@users.noreply.github.com>
2025-03-05 14:00:50 +0900

    [Bronze V] Title: 웰컴, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/5337. 웰컴/웰컴.cc
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(void)
+{
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+
+  cout << ".  .   .\n";
+  cout << "|  | _ | _. _ ._ _  _\n";
+  cout << "|/\\|(/.|(_.(_)[ | )(/.";
+
+}
+
+
