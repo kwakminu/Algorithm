076504023775793eb574826ca54edf6e99c29bac
KMW <129249419+kwakminu@users.noreply.github.com>
2025-03-06 17:47:08 +0900

    [Bronze V] Title: 단어 길이 재기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/2743. 단어 길이 재기/단어 길이 재기.cc
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(void)
+{
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+
+  string k;
+  cin >> k;
+  int cnt;
+  for (char i : k) {
+    cnt ++;
+  }
+  cout << cnt;
+}
