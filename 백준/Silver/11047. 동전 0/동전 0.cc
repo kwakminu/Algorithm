640d1898f76677e471fc79fd19548c164ee0ea0c
KMW <129249419+kwakminu@users.noreply.github.com>
2025-05-11 14:09:56 +0900

    [Silver IV] Title: 동전 0, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/11047. 동전 0/동전 0.cc
+++ 백준/Silver/11047. 동전 0/동전 0.cc
@@ -1,7 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int n, k;
+int N, k;
 int a[15];
 
 int main(void){
@@ -9,9 +9,9 @@ int main(void){
   cin.tie(0);
 
   int ans = 0;
-  cin >> n >> k;
-  for (int i = 0; i < n; i++) cin >> a[i];
-  for (int i = n-1; i >=0; i--){
+  cin >> N >> k;
+  for (int i = 0; i < N; i++) cin >> a[i];
+  for (int i = N-1; i >=0; i--){
     ans += k / a[i];
     k = k % a[i];
   }
