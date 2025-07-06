281e9f9e37a6eb173a5920d84e1f91035c55db4a
KMW <129249419+kwakminu@users.noreply.github.com>
2025-07-06 14:47:28 +0900

    [Bronze IV] Title: 나는 행복합니다~, Time: 0 ms, Memory: 2136 KB -BaekjoonHub

---
+++ 백준/Bronze/14652. 나는 행복합니다～/나는 행복합니다～.cc
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int arr[30005];
+
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int N, M, K;
+    cin >> N >> M>> K;
+
+    cout << K/M << ' ' << K%M;
+
+    
+
+
+    
+
+
+}
