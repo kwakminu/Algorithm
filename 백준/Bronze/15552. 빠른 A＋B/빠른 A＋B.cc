76e8d390ce61c0661f22e29ed2f612fd59681fb9
KMW <129249419+kwakminu@users.noreply.github.com>
2025-07-09 15:42:06 +0900

    [Bronze IV] Title: 빠른 A+B, Time: 196 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/15552. 빠른 A＋B/빠른 A＋B.cc
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int T;
+    cin >> T;
+    for (int i = 0; i < T; i++) {
+        int A, B;
+        cin >> A >> B;
+        cout << A + B << '\n';
+    }
+
+
+}
