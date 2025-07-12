baf53b9c540473e8f20194195025c911b7211830
KMW <129249419+kwakminu@users.noreply.github.com>
2025-07-12 15:07:50 +0900

    [Bronze V] Title: 기찍 N, Time: 8 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2742. 기찍 N/기찍 N.cc
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int N;
+    cin >> N;
+    for (int i = N; i > 0; i--) {
+        cout << i << '\n';
+    }
+} 
