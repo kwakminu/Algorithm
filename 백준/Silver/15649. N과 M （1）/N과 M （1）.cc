0d8136e31f08a340ceecc81db08ae841745ff033
KMW <129249419+kwakminu@users.noreply.github.com>
2025-07-17 16:48:05 +0900

    [Silver III] Title: N과 M (1), Time: 20 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.cc
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.cc
@@ -1,32 +1,35 @@
 #include <bits/stdc++.h>
+
 using namespace std;
 
-int n, m;
 int arr[10];
-bool isused[10];
+bool used[10];
+int n, m;
 
-void func(int k){ // 여기서 k : 현재까지 k개의 수를 택했다. 
-  if (k == m){
-    for (int i = 0; i < m; i++){
-      cout << arr[i] << ' ';
+void func(int k) {
+    if (k == m) {
+        for (int i = 0; i < m; i ++) {
+            cout << arr[i] << ' ';
+        }
+        cout << '\n';
+        return;
     }
-    cout << '\n';
-    return;
-  }
-
-  for (int i = 1; i <= n; i++){
-    if (!isused[i]){
-      arr[k] = i;
-      isused[i] = true;
-      func(k+1);
-      isused[i] = false; // 다 끝났으니까! k번째 수를 i로 정한 모든 경우에 대해 다 확인했으니 i를 이제 사용되지 않았다고 명시한다. 
+    for (int i = 1; i <= n; i++){
+        if (used[i] == false) {
+            arr[k] = i;
+            used[i] = true;
+            func(k+1);
+            used[i] = false;
+        }
     }
-  }
 }
 
-int main(void){
-  ios::sync_with_stdio(0);
-  cin.tie(0);
-  cin >> n >> m;
-  func(0);
-}
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    cin >> n >> m;
+    func(0);
+} 
