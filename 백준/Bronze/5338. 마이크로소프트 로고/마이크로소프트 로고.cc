0232873e8b1a57dd25c29982889ff8183454afe7
KMW <129249419+kwakminu@users.noreply.github.com>
2025-07-20 16:48:02 +0900

    [Bronze V] Title: 마이크로소프트 로고, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/5338. 마이크로소프트 로고/마이크로소프트 로고.cc
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    cout << "       _.-;;-._\n";
+    cout << "\'-..-\'|   ||   |\n";
+    cout << "\'-..-\'|_.-;;-._|\n";
+    cout << "\'-..-\'|   ||   |\n";
+    cout << "\'-..-\'|_.-\'\'-._|";
+    
+    
+} 
