6c61abbe6efc6f518864f2960eb0fda2ce7a3a07
KMW <129249419+kwakminu@users.noreply.github.com>
2025-08-04 19:27:46 +0900

    [Bronze V] Title: 와이버스 부릉부릉, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/14645. 와이버스 부릉부릉/와이버스 부릉부릉.cc
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int n, a;
+    cin >> n >> a;
+    for (int i = 0; i < n; i ++) {
+        int k, j;
+        cin >> k >> j;
+    }
+    cout << "비와이";
+}
