3981b0c6efa18ccb95ae7c1dc6f3f0b404ccc958
KMW <129249419+kwakminu@users.noreply.github.com>
2025-08-07 21:11:38 +0900

    [Bronze V] Title: 삼각형, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/29751. 삼각형/삼각형.cc
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    double a, b, c;
+    cin >> a >> b;
+    printf("%.1f", a*b*1/2);
+}
