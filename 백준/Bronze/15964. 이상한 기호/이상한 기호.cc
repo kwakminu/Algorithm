f06fdc0b1b17edab1fc5c9cbe29b74e61e51eef4
KMW <129249419+kwakminu@users.noreply.github.com>
2025-08-11 15:52:08 +0900

    [Bronze V] Title: 이상한 기호, Time: 0 ms, Memory: 2020 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15964. 이상한 기호/이상한 기호.cc
+++ 백준/Bronze/15964. 이상한 기호/이상한 기호.cc
@@ -6,9 +6,9 @@ int main()
     ios::sync_with_stdio(0);
     cin.tie(0);
 
-    int a, b;
+    long long a, b;
     cin >> a >> b;
-    int c; 
+    long long c; 
     c = (a+b) * (a-b);
     cout << c;
 }
