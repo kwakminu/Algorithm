09afe92d38673bd36587da1fc45a7b99f34732a2
KMW <129249419+kwakminu@users.noreply.github.com>
2025-08-27 20:33:42 +0900

    [Bronze V] Title: Cupcake Party, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/24568. Cupcake Party/Cupcake Party.cc
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    
+    int a, b;
+    cin >> a >> b;
+    int sum;
+    sum = a * 8 + b * 3;
+    cout << sum - 28;
+
+    return 0;
+}
